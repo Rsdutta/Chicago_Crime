aff8c40f6c1c2855138b75e3b2cf3b435966cf8f
Rsdutta <rsdutta2@illinois.edu>
2018-04-15 23:19:09 -0500

    table setup done and unique crime tags done

--- table_setup.py
+++ table_setup.py
@@ -11,16 +11,17 @@ def setup_table(sc, sqlContext, reviews_filename):
     Parse the reviews file and register it as a table in Spark SQL in this function
     '''
     df = sqlContext.read.csv(reviews_filename, header=True, inferSchema=True)
+    #begin preprocessing to get rid of null values
+    df = df.dropna(how='any', thresh=None, subset=None)
+    df = df.drop_duplicates()
     sqlContext.registerDataFrameAsTable(df, "crime_data")
-    df.show()
+
 
 
 if __name__ == '__main__':
-
 	file = "hdfs:///projects/group14/crimedata.csv"
-    # Setup Spark
-    conf = SparkConf().setAppName("crime_data_stats")
-    sc = SparkContext(conf=conf)
-    sqlContext = SQLContext(sc)
-
-    setup_table(sc, sqlContext, file)
+	# Setup Spark
+	conf = SparkConf().setAppName("crime_data_stats")
+	sc = SparkContext(conf=conf)
+	sqlContext = SQLContext(sc)
+	setup_table(sc, sqlContext, file)
