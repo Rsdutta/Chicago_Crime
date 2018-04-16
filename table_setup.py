ae632b2d47a108307c3a1fcbd1d9678e532a0af7
Rsdutta <rsdutta2@illinois.edu>
2018-04-15 22:01:28 -0500

    initial commit to setup table

---
+++ table_setup.py
@@ -0,0 +1,26 @@
+from pyspark import SparkContext, SparkConf
+from pyspark.sql import SQLContext
+
+
+def setup_table(sc, sqlContext, reviews_filename):
+    '''
+    Args:
+        sc: The Spark Context
+        sqlContext: The Spark SQL context
+        reviews_filename: Filename of the Amazon reviews file to use, where each line represents a review    
+    Parse the reviews file and register it as a table in Spark SQL in this function
+    '''
+    df = sqlContext.read.csv(reviews_filename, header=True, inferSchema=True)
+    sqlContext.registerDataFrameAsTable(df, "crime_data")
+    df.show()
+
+
+if __name__ == '__main__':
+
+	file = "hdfs:///projects/group14/crimedata.csv"
+    # Setup Spark
+    conf = SparkConf().setAppName("crime_data_stats")
+    sc = SparkContext(conf=conf)
+    sqlContext = SQLContext(sc)
+
+    setup_table(sc, sqlContext, file)
