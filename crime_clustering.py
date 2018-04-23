e81edd0e290261959138af58d6ae2206b2d33e53
Rsdutta <rsdutta2@illinois.edu>
2018-04-23 01:59:05 -0500

    clustering done

--- crime_clustering.py
+++ crime_clustering.py
@@ -6,13 +6,18 @@ from pyspark.ml.clustering import KMeans, KMeansModel
 from pyspark.ml.feature import VectorAssembler
 import folium
 from folium.plugins import MarkerCluster
+from datetime import datetime
 
 
 def create_clusters(sqlContext):
-	specific_data = sqlContext.sql('SELECT Description, Date, Latitude, Longitude FROM crime_data where `Primary Type` == "HOMOCIDE" or `Primary Type` == "ASSAULT" ')
-
+	specific_data = sqlContext.sql('SELECT Description, Date, Latitude, Longitude FROM crime_data')
+	#where `Primary Type` == "HOMOCIDE" or `Primary Type` == "ASSAULT" 
 	specific_data.filter(specific_data.Description != "SIMPLE")
-	specific_data = specific_data.sample(False, .01)
+
+	specific_data =  specific_data.rdd.filter(lambda x: datetime.strptime(x[1], "%m/%d/%Y %I:%M:%S %p").weekday() in [5, 6]).toDF()
+
+
+	specific_data = specific_data.sample(False, .0001)
 	#create different k-means outputs depending on the date since that is what we are testing
 
 	vecAssembler = VectorAssembler(inputCols=['Latitude', 'Longitude'], outputCol="features")
@@ -60,6 +65,6 @@ def create_visuals(df):
 	locationlist = locationslist.collect()
 	for index in range(0, len(locationlist)):
 		folium.Marker([locationlist[index][1][0], locationlist[index][1][1]], popup=locationlist[index][0][0]).add_to(marker_cluster)
-	chicago_map.save('severe.html')
+	chicago_map.save('weekend.html')
 
 
