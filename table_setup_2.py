2a7d641e4ced093b2d38fb133afd22d6e1c169cd
Rsdutta <rsdutta2@illinois.edu>
2018-05-02 18:56:50 -0500

    readme and my part done

---
+++ table_setup_2.py
@@ -0,0 +1,47 @@
+from pyspark import SparkContext, SparkConf
+from pyspark.sql import SQLContext
+from crime_clustering import create_clusters
+from crime_statistics import *
+from daily_crime import *
+from heatmap import *
+
+##########################################################################################
+# This file is the main file where we will be adding functions to see results of our     #
+# code. Add functions you want to run after setup_table. Other than that add new         #
+# functions to different files and import them as i did above with crime_clustering, etc #
+# in order to keep this document clutter free. There are TODO's below with things we need#
+# to work on.                                                                            #   
+##########################################################################################
+
+def setup_table(sc, sqlContext, reviews_filename):
+    '''
+    Args:
+        sc: The Spark Context
+        sqlContext: The Spark SQL context
+        reviews_filename: Filename of the Amazon reviews file to use, where each line represents a review    
+    Parse the reviews file and register it as a table in Spark SQL in this function
+    '''
+    df = sqlContext.read.csv(reviews_filename, header=True, inferSchema=True)
+    #begin preprocessing to get rid of null values
+    df = df.dropna(how='any', thresh=None, subset=None)
+    df = df.drop_duplicates()
+    df = df.filter(df.Description != "SIMPLE")
+    sqlContext.registerDataFrameAsTable(df, "crime_data")
+
+
+
+if __name__ == '__main__':
+
+    file = "hdfs:///projects/group14/crimedata.csv"
+    # Setup Spark
+    conf = SparkConf().setAppName("crime_data_stats")
+    sc = SparkContext(conf=conf)
+    sqlContext = SQLContext(sc)
+    setup_table(sc, sqlContext, file)
+    create_clusters(sqlContext)
+    create_heatmap(sqlContext)
+    
+
+    
+
+
